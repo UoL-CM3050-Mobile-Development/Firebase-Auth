4a4d97e5a45db14cf62f6c256f62d9e25abab44e
Volodymyr Kurov <vokur13@gmail.com>
2023-03-10 20:31:37 +0200

    navigation +

--- my-app/App.js
+++ my-app/App.js
@@ -1,47 +1,32 @@
-import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from './components/login';
-import Signup from './components/signup';
-import Dashboard from './components/dashboard';
-const Stack = createStackNavigator();
-function MyStack() {
-  return (
-    <Stack.Navigator
-      initialRouteName="Signup"
-      screenOptions={{
-        headerTitleAlign: 'center',
-        headerStyle: {
-          backgroundColor: '#3740FE',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}
-    >
-      <Stack.Screen
-        name="Signup"
-        component={Signup}
-        options={{ title: 'Signup' }}
-      />
-      <Stack.Screen
-        name="Login"
-        component={Login}
-        options={({ title: 'Login' }, { headerLeft: null })}
-      />
-      <Stack.Screen
-        name="Dashboard"
-        component={Dashboard}
-        options={({ title: 'Dashboard' }, { headerLeft: null })}
-      />
-    </Stack.Navigator>
-  );
-}
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
-      <MyStack />
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

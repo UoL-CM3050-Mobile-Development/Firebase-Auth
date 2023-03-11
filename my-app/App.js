53ab9804b5b776c1369d25ac2eaf4d1b7fad6ad7
Volodymyr Kurov <vokur13@gmail.com>
2023-03-11 12:50:20 +0200

    Update App.js

--- my-app/App.js
+++ my-app/App.js
@@ -1,4 +1,3 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
@@ -20,12 +19,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
